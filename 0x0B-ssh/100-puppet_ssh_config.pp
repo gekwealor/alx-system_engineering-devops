d6cbe695f8f02c71551bcce1ace64171612ec035
gekwealor <gekwealor@gmail.com>
2023-10-31 10:56:58 -0400

    Config puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
-# Puppet script to change client config file ssh
+# puppet to connect without password
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#PasswordAuthentication yes',
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'PasswordAuthentication no',
+  match =>  '^#PasswordAuthentication',
 }
 
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/id_rsa',
-  match => '^#   IdentityFile ~/.ssh/id_rsa',
-}
-
-file { '/home/vagrant/.ssh/id_rsa':
-  source  => '/vagrant/id_rsa',
-  mode    => '0600',
-  owner   => 'vagrant',
-  group   => 'vagrant',
-}
-
-class { 'ssh':
-  service_hasstatus => false,
-  service_restart   => '/etc/init.d/ssh reload',
-  service_name      => 'ssh',
+file_line { 'Decare identity file':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'IdentityFile ~/.ssh/school',
+  match =>  '^#IdentityFile',
 }
